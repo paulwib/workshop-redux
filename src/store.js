b0025bd29db11399138db9d6573f3311d9169460
Paul Willoughby <paul@fivetide.com>
2016-12-21 01:17:15 +0000

    Add exercise on handling rejected promises

--- src/store.js
+++ src/store.js
@@ -1,7 +1,8 @@
 const redux = require('redux');
+const thunkMiddleware = require('redux-thunk').default;
 const promiseMiddleware = require('redux-promise-middleware').default;
 const reducer = require('./reducers');
 
 module.exports = function (initialItems) {
-	return redux.createStore(reducer, { items: initialItems }, redux.applyMiddleware(promiseMiddleware()));
+	return redux.createStore(reducer, { items: initialItems }, redux.applyMiddleware(thunkMiddleware, promiseMiddleware()));
 };
